2dbe56e5d01d48ae6176ed1ecf539430288a3c04
prajankumar <mr.prajankumar001@gmail.com>
2025-03-16 14:09:56 +0530

    adding all files of frontend to git

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const App = () => {
+  return (
+    <div>
+    <Navbar></Navbar>
+    <Login></Login>
+    <Register></Register>
+          </div>
+  )
+}
+
+export default App
